fc03e00fb2843379fe943e89b1d9a5694ca97c43
gsh519 <gsh05196yu@yahoo.co.jp>
2021-12-26 16:42:36 +0900

    サンプルトップページ

--- index.php
+++ index.php
@@ -5,13 +5,110 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>クイズメーカー</title>
-  <link rel="stylesheet" href="reset.css">
-  <link rel="stylesheet" href="style.min.css">
+  <title>なぞなぞメーカー</title>
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.min.css">
 </head>
 
 <body>
-  <h1>ようこそクイズメーカーへ</h1>
+  <main>
+    <div class="wrapper">
+      <div class="hero">
+        <h1 class="hero__ttl">なぞなぞメーカー</h1>
+        <p class="hero__note">こたえて遊ぶ ✕ 作って遊ぶ</p>
+        <div class="hero__flex">
+          <a class="hero__flex__link" href="#">なぞなぞを答える</a>
+          <a class="hero__flex__link" href="#">なぞなぞを作る</a>
+        </div>
+      </div>
+
+      <div class="questions-list">
+        <h2 class="questions-list__ttl">人気のなぞなぞ</h2>
+        <ul class="questions-list__flex">
+
+          <li class="list">
+            <a href="#">
+              <div class="list__question">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</div>
+              <div class="list__des">
+                <p>作成者名</p>
+                <p>2021/8/17</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="list">
+            <a href="#">
+              <div class="list__question">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</div>
+              <div class="list__des">
+                <p>作成者名</p>
+                <p>2021/8/17</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="list">
+            <a href="#">
+              <div class="list__question">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</div>
+              <div class="list__des">
+                <p>作成者名</p>
+                <p>2021/8/17</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="list">
+            <a href="#">
+              <div class="list__question">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</div>
+              <div class="list__des">
+                <p>作成者名</p>
+                <p>2021/8/17</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="list">
+            <a href="#">
+              <div class="list__question">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</div>
+              <div class="list__des">
+                <p>作成者名</p>
+                <p>2021/8/17</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="list">
+            <a href="#">
+              <div class="list__question">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</div>
+              <div class="list__des">
+                <p>作成者名</p>
+                <p>2021/8/17</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="list">
+            <a href="#">
+              <div class="list__question">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</div>
+              <div class="list__des">
+                <p>作成者名</p>
+                <p>2021/8/17</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="list">
+            <a href="#">
+              <div class="list__question">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</div>
+              <div class="list__des">
+                <p>作成者名</p>
+                <p>2021/8/17</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
